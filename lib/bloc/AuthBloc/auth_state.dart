66768ce99f0f412379b2fb55ed9de5f31b03b842
pugazhmukilan <pugazhmukilanoffical2004@gmail.com>
2024-06-19 12:59:07 +0530

    bloc update

--- lib/bloc/AuthBloc/auth_state.dart
+++ lib/bloc/AuthBloc/auth_state.dart
@@ -4,3 +4,16 @@ part of 'auth_bloc.dart';
 sealed class AuthState {}
 
 final class AuthInitial extends AuthState {}
+final class AuthFailed extends AuthState{
+  String errorMessage;
+  AuthFailed({required this.errorMessage});
+
+}
+
+final class AuthSuccessfull extends AuthState{
+  String successMessage;
+  AuthSuccessfull({required this.successMessage});
+}
+final class AuthLoading extends AuthState{
+  
+}
